ca0c033781eaa201125c3555ca712841dbb6aa44
张哥 <zandy@zhanggedeMacBook-Pro.local>
2021-11-24 15:17:24 +0800

    first commit

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,15 +1,18 @@
 sudo apt install unzip wget -y
 # awscli
+# https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html
 sudo snap remove aws-cli
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
 # gcloud
+# https://cloud.google.com/sdk/docs/install#deb
 sudo apt-get install apt-transport-https ca-certificates gnupg
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 sudo apt-get update && sudo apt-get install google-cloud-sdk -y
 # docker
+# https://docs.docker.com/engine/install/ubuntu/
 sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get install \
     ca-certificates \
@@ -23,5 +26,6 @@ echo \
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 # azure-cli
+# https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-linux?pivots=apt
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
