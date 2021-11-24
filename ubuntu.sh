9274743a4b1b9edbd8dc501473ac4b4645ee2fb0
张哥 <zandy@zhanggedeMacBook-Pro.local>
2021-11-24 15:15:22 +0800

    first commit

--- ubuntu.sh
+++ ubuntu.sh
@@ -22,4 +22,6 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+# azure-cli
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
